3726bf342dc1c335c5abdd70496c00b18864e81f
Sokolov Denis <denalfek@gmail.com>
2023-12-01 15:52:54 +0400

    create client

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var type = Triangles.TypeDetector.DetectType(1,2, 3);
+var type1 = Triangles.TypeDetector.DetectType(1,2, 0);
+Console.WriteLine(type);
+Console.WriteLine(type1.AsT1.Value);
