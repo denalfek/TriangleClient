9b9402be2a995f7b3272f86ae41febfcdf2562ed
Sokolov Denis <denalfek@gmail.com>
2023-12-01 15:57:58 +0400

    use case

--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-var type = Triangles.TypeDetector.DetectType(1,2, 3);
-var type1 = Triangles.TypeDetector.DetectType(1,2, 0);
-Console.WriteLine(type);
-Console.WriteLine(type1.AsT1.Value);
+var firstType = Triangles.TypeDetector.DetectType(3,4, 5);
+var secondType = Triangles.TypeDetector.DetectType(1.55M,14.0134M, 2.5M);
+var error = Triangles.TypeDetector.DetectType(1,2, 0);
+Console.WriteLine($"First type: {firstType.AsT0}");
+Console.WriteLine($"Second type: {secondType.AsT0}");
+Console.WriteLine(error.AsT1.Value);
